b1e0fc1eb7b863d1f4dc107401f13fda8aa44631
Fernando Pires <fomeboy@yahoo.com>
2017-03-24 19:28:49 +0000

    Enzyme shallow testing

--- js/RoutingPage.spec.js
+++ js/RoutingPage.spec.js
@@ -2,9 +2,17 @@
 import React from 'react'
 import RoutingPage from './RoutingPage.js'
 import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
+import { shallowToJson } from 'enzyme-to-json'
 
-test('Routing page snapshot teste', () => {
+test('Routing page snapshot test', () => {
   const component = renderer.create(<RoutingPage/>)
   const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+test('Routing page enzyme test', () => {
+  const component = shallow(<RoutingPage/>)
+  const tree = shallowToJson(component)
   expect(tree).toMatchSnapshot()
 })
